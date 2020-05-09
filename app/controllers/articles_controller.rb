1917d76982d929338ac1ed1368645fcccec44e67
nikolatriki <kjofte@yahoo.com>
2020-05-09 21:23:20 +0200

    Create action Update, view Edit

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -12,13 +12,27 @@ class ArticlesController < ApplicationController
   end
 
   def create
-    binding.pry
     @article = Article.new(article_params)
     if @article.save
       redirect_to @article # redirect_to "/articles/#{article.id}"
     else
       render :new
     end
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render :edit
+    end
+
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,11 @@ Rails.application.routes.draw do
 
   get '/articles/:id', to: 'articles#show', as: :article
 
+  get '/articles/:id/edit', to: 'articles#edit', as: :edit_article
+
+  patch '/articles/:id', to: 'articles#update'
+
   root(to: 'articles#index')
-  
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
