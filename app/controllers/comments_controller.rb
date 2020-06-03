5dcdae3a6ddc04c27190ba237e50fa955e48f707
nikolatriki <kjofte@yahoo.com>
2020-06-03 21:58:31 +0200

    Generate Comments controler and Comment model, redesigned Articles controler, views and model

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,22 @@
+class CommentsController < ApplicationController
+  def create
+    article = Article.find(params[:article_id])
+
+    comment = article.comments.build(comment_params)
+
+    comment.save
+    redirect_to article
+  end
+
+  def destroy
+    comment = Comment.find(params[:id])
+    comment.destroy
+
+    redirect_to comment.article
+  end
+
+  private
+  def comment_params
+    params.require(:comment).permit(:commenter, :body)
+  end
+end
