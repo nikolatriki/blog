2b1944412f78ea4c2295ad4aee1d48772ba7308a
nikolatriki <kjofte@yahoo.com>
2020-07-03 23:22:49 +0200

    Refactoring comments_controler, session_helper, models and views with added User relation

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,5 +1,7 @@
 class CommentsController < ApplicationController
   def new
+    session_notice(:danger, 'You must be logged in!', login_path) unless logged_in?
+
     @article = Article.find(params['article_id'])
     @comment = @article.comments.build
   end
@@ -7,6 +9,7 @@ class CommentsController < ApplicationController
   def create
     @article = Article.find(params['article_id'])
     @comment = @article.comments.build(comment_params)
+    @comment.user = current_user
 
     if @comment.save
       redirect_to @article
@@ -24,7 +27,11 @@ class CommentsController < ApplicationController
   end
 
   def edit
+    session_notice(:danger, 'You must be logged in!', login_path) unless logged_in?
+
+
     @comment = Comment.find(params[:id])
+    session_notice(:danger, 'Wrong user!') unless equal_with_current_user?(@comment.user)
     # @article = Article.find(params[:article_id])
     @article = @comment.article
   end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,18 +1,22 @@
 class User < ApplicationRecord
+  MAXIMUM_NAME_LENGTH = 255
+  MAXIMUM_EMAIL_LENGTH = 50
+  MINIMUM_PASSWORD_LENGTH = 6
 
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
 
   has_secure_password
 
   has_many :articles, dependent: :destroy
+  has_many :comments, dependent: :destroy
 
   before_save :email_to_downcase
 
-  validates :name, presence: true, length: { maximum: 50}
-  validates :email, presence: true, length: { maximum: 255 },
+  validates :name, presence: true, length: { maximum: MAXIMUM_NAME_LENGTH}
+  validates :email, presence: true, length: { maximum: MAXIMUM_EMAIL_LENGTH},
                     format: { with: VALID_EMAIL_REGEX },
                     uniqueness: true
-  validates :password, presence: true, length: { minimum: 6}
+  validates :password, presence: true, length: { minimum: MINIMUM_PASSWORD_LENGTH}
 
   private
 
